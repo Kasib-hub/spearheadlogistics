2cc65b81c79b27ecdc170db4c90b34ab2f559c01
Kasib <kasibabdullah8@gmail.com>
2023-04-22 12:56:01 -0400

    pushing changes to deployed site

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,10 +1,21 @@
+import logo from '../assets/logo.jpeg'
+import './Cards.css'
+
 function Cards() {
 
   return (
     <>
+    <div className='box-row'>
       <div className="card">
-        
+        <img src={logo} alt="spearhead logo" className="logo"/>
+        <p className='label'>Mission</p>
       </div>
+      <div className="card">
+        <img src={logo} alt="spearhead logo" className="logo"/>
+        <p className='label'>Mission</p>
+      </div>
+    </div>
+
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 
 import logo from '../assets/logo.jpeg'
+import Cards from '../components/Cards';
 // homepage will have a slew of buttons to click on that navigate to other pages
 // so it will be a card=div with an image, title, and as a link
 
@@ -10,7 +11,8 @@ function HomePage() {
       <h1 className="title">Spearhead of All Logistics</h1>
       <p className='p-text'>A non-emergency medical transportation business in San Antonio, Texas.</p>
       <p className='p-text'>"You Don't Wait On Us, We Wait On You."</p>
-      <img src={logo} alt="spearhead logo" className="logo"/>
+      <Cards />
+
       {/* import the card */}
     </>
   );
