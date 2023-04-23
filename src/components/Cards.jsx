847f3c8e2cd2df15e3f93e55fe1859a7b9ac2604
Kasib <kasibabdullah8@gmail.com>
2023-04-23 14:30:29 -0400

    styled cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,9 @@
-import logo from '../assets/logo.jpeg'
+import Logo from '../assets/smallLogo.png'
+import Vision from '../assets/vision.png'
+import Employment from '../assets/employment.png'
+import Training from '../assets/training.png'
+import Values from '../assets/values.png'
+import Service from '../assets/service.png'
 import './Cards.css'
 
 function Cards() {
@@ -7,12 +12,28 @@ function Cards() {
     <>
     <div className='box-row'>
       <div className="card">
-        <img src={logo} alt="spearhead logo" className="logo"/>
+        <img src={Logo} alt="spearhead logo" className="card-img"/>
         <p className='label'>Mission</p>
       </div>
       <div className="card">
-        <img src={logo} alt="spearhead logo" className="logo"/>
-        <p className='label'>Mission</p>
+        <img src={Vision} alt="longroad" className="card-img"/>
+        <p className='label'>Vision</p>
+      </div>
+      <div className="card">
+        <img src={Values} alt="longroad" className="card-img"/>
+        <p className='label'>Values</p>
+      </div>
+      <div className="card">
+        <img src={Service} alt="manLooking" className="card-img"/>
+        <p className='label'>Services</p>
+      </div>
+      <div className="card">
+        <img src={Employment} alt="truck_driver" className="card-img"/>
+        <p className='label'>Employment</p>
+      </div>
+      <div className="card">
+        <img src={Training} alt="truck_driver" className="card-img"/>
+        <p className='label'>Training</p>
       </div>
     </div>
 
