dadff2066334bee3514366b33a458cf965474327
Kasib <kasibabdullah8@gmail.com>
2023-04-23 19:26:47 -0400

    training page

--- src/pages/TrainingPage.jsx
+++ src/pages/TrainingPage.jsx
@@ -1,8 +1,35 @@
+import Header from '../components/Header';
+import { useNavigate } from 'react-router-dom';
+
 function TrainingPage() {
+
+  const navigate = useNavigate();
 
   return (
     <>
-      <h1>Employment Page</h1>
+      <Header 
+        titleText='Training'
+      />
+      <p><strong>TRAINING AND CERTIFICATIONS</strong></p>
+      <ul>
+        <li>Adult/Child First Aid and CPR</li>
+        <li>Defensive Driving</li>
+        <li>Passenger Assistance and Safety</li>
+        <li>Lift and Transfer Training</li>
+        <li>Wheelchair and Mobility Assistance</li>
+        <li>ADA (American Disability ACT) Understanding Clients With Disabilities</li>
+        <li>Fire Safety</li>
+        <li>Disaster Preparedness</li>
+        <li>Client Sensitivity/Cultural Awareness</li>
+        <li>Sexual Harassment and Response Training</li>
+        <li>Client Confidentiality (HIPPA)</li>
+        <li>Mandated Reporting</li>
+        <li>Car-Seat Safety</li>
+        <li>Patient Communication</li>
+        <li>Emergency Response</li>
+        <li>Company Policies and Procedures</li>
+      </ul>
+      <button className='return-btn' onClick={() => navigate('/')}>Go Back</button>
     </>
   );
 }
