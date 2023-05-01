4966ba92484db26969a890d78512893fdffea037
Kasib <kasibabdullah8@gmail.com>
2023-04-30 21:24:16 -0400

    added contac info to bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import Phone from '../assets/phone.svg'
+import Email from '../assets/email.svg'
+import Marker from '../assets/marker.svg'
 import './Footer.css'
 
 function Footer() {
@@ -6,7 +8,17 @@ function Footer() {
   return (
     <>
       <div className='contact'>
-        <img src={Phone} alt='phone' /><span>*phone here*</span>
+        <p><strong>Contact</strong></p>
+        <div>
+          <img src={Phone} alt='phone' /><span>210-419-6216</span>
+        </div>
+        <div>
+          <img src={Email} alt='email' /><span>spearheadofalllogistics@gmail.com</span>
+        </div>
+        <div>
+          <img src={Marker} alt='marker' /><span>San Antonio, Texas</span>
+        </div>
+        
       </div>
       
     </>
